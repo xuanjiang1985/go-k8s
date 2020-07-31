e990807a334f406c431267f9129dce9d56ce2168
Yzhougang <sogongyu@163.com>
2020-07-31 15:57:10 +0800

    docker build images bash

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
-# change the image tag before building
+#!/bin/bash
 
-docker build -t goappk8s:v0.4 .
+# change the image tag before building
+APP_VERSION=v0.4.2
+
+docker build -t goappk8s:${APP_VERSION} .
 
 docker login --username=zhuxmei520@163.com registry.cn-hangzhou.aliyuncs.com
 
-docker tag goappk8s:v0.4 registry.cn-hangzhou.aliyuncs.com/zgang/goappk8s:v0.4
+docker tag goappk8s:${APP_VERSION} registry.cn-hangzhou.aliyuncs.com/zgang/goappk8s:${APP_VERSION}
 
-docker push registry.cn-hangzhou.aliyuncs.com/zgang/goappk8s:v0.4
+docker push registry.cn-hangzhou.aliyuncs.com/zgang/goappk8s:${APP_VERSION}
